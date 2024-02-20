5c041df2fe34bf02263fc06a6cd4e183110e86f3
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-20 13:17:32 +0200

    adv tasks

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,95 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t mec, char pth);
+void bitonic_seq(int *array, size_t size, size_t start, size_t mec, char pth);
+void bitonic_sort(int *array, size_t size);
+
+/**
+ * swap_ints - this func will re-order integers
+ * @a: 1st int
+ * @b: 2nd int
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bitonic_merge - this func will re_order the [] based_on bitonic algo
+ *
+ * @array: [int]
+ *
+ * @size: [size]
+ *
+ * @start: point of srt
+ *
+ * @mec: [siz]
+ *
+ * @pth: the pth will be taken
+ *
+ */
+
+void bitonic_merge(int *array, size_t size, size_t start, size_t mec, char pth)
+{
+	size_t xml, tke = mec / 2;
+
+	if (mec > 1)
+	{
+		for (xml = start; xml < start + tke; xml++)
+		{
+			if ((pth == UP && array[xml] > array[xml + tke]) ||
+			    (pth == DOWN && array[xml] < array[xml + tke]))
+				swap_ints(array + xml, array + xml + tke);
+		}
+		bitonic_merge(array, size, start, tke, pth);
+		bitonic_merge(array, size, start + tke, tke, pth);
+	}
+}
+
+/**
+ * bitonic_seq - Convert an array of integers into a bitonic sequence.
+ * @array: [int]
+ * @size: [size]
+ * @start: The starting index of a block of the building bitonic sequence.
+ * @mec: The size of a block of the building bitonic sequence.
+ * @pth: The direction to sort the bitonic sequence block in.
+ */
+void bitonic_seq(int *array, size_t size, size_t start, size_t mec, char pth)
+{
+	size_t cut = mec / 2;
+	char *str = (pth == UP) ? "UP" : "DOWN";
+
+	if (mec > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", mec, size, str);
+		print_array(array + start, mec);
+
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, mec, pth);
+
+		printf("Result [%lu/%lu] (%s):\n", mec, size, str);
+		print_array(array + start, mec);
+	}
+}
+
+/**
+ * bitonic_sort - re_order the [] based_on bitonic algo
+ *
+ * @array: [int]
+ *
+ * @size: [size]
+ *
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	bitonic_seq(array, size, 0, size, UP);
+}
