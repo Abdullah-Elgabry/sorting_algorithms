1196fe825d22e314dd46aba2ec9cf49ddb6a6847
“abdullah <abdullah.h.elgabry@gmail.com>
2024-02-20 12:30:36 +0200

    adv tasks

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,33 +1,5 @@
 #include "sort.h"
 
-
-/**
- * get_max - this func will return the max int val
- *
- * @array: [int]
- *
- * @size: [size]
- *
- * Return: max int
- *
- */
-
-int get_max(int *array, int size)
-{
-    int lg, xml;
-
-
-    for (lg = array[0], xml = 1; xml < size; xml++)
-    {
-        if (array[xml] > lg)
-            lg = array[xml];
-    }
-
-
-    return (lg);
-}
-
-
 /**
  * counting_sort - re-order the int based_on counting algo
  *
@@ -39,45 +11,40 @@ int get_max(int *array, int size)
 
 void counting_sort(int *array, size_t size)
 {
-    int *inc, *rearr, lg, xml;
-
-
-    if (array == NULL || size < 2)
-        return;
-
-
-    rearr = malloc(sizeof(int) * size);
-    if (rearr == NULL)
-        return;
-    lg = get_max(array, size);
-    inc = malloc(sizeof(int) * (lg + 1));
-    if (inc == NULL)
-    {
-        free(rearr);
-        return;
-    }
-
-
-    for (xml = 0; xml < (lg + 1); xml++)
-        inc[xml] = 0;
-    for (xml = 0; xml < (int)size; xml++)
-        inc[array[xml]] += 1;
-    for (xml = 0; xml < (lg + 1); xml++)
-        inc[xml] += inc[xml - 1];
-    print_array(inc, lg + 1);
-
-
-    for (xml = 0; xml < (int)size; xml++)
-    {
-        rearr[inc[array[xml]] - 1] = array[xml];
-        inc[array[xml]] -= 1;
-    }
-
-
-    for (xml = 0; xml < (int)size; xml++)
-        array[xml] = rearr[xml];
-
-
-    free(rearr);
-    free(inc);
+int itrl;
+size_t i;
+int *sorted_array;
+int *counting_array;
+if (array == NULL || size <= 1)
+return;
+itrl = array[0];
+for (i = 1; i < size; i++)
+{
+if (array[i] > itrl)
+itrl = array[i];
+}
+counting_array = malloc((itrl + 1) * sizeof(int));
+if (counting_array == NULL)
+return;
+for (i = 0; i <= (size_t)itrl; i++)
+counting_array[i] = 0;
+for (i = 0; i < size; i++)
+counting_array[array[i]]++;
+print_array(counting_array, itrl + 1);
+printf("\n");
+sorted_array = malloc(size * sizeof(int));
+if (sorted_array == NULL)
+{
+free(counting_array);
+return;
+}
+for (i = 0; i < size; i++)
+{
+sorted_array[counting_array[array[i]] - 1] = array[i];
+counting_array[array[i]]--;
+}
+for (i = 0; i < size; i++)
+array[i] = sorted_array[i];
+free(counting_array);
+free(sorted_array);
 }
